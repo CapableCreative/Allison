5b8bf2ad806bac309576b47e055ca09bc35b368c
Stephen Fox <stephen@capablecreative.com>
2020-01-06 22:51:39 -0600

    Server adjustments - SLF

--- server/index.js
+++ server/index.js
@@ -18,6 +18,10 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 }
 
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 app.listen(PORT, () =>
   console.log('Express server is running on localhost:3001')
 );
