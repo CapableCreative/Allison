7ff8392f64585b7b4aea4b0e2dd2080d19dfdbed
Stephen Fox <stephen@capablecreative.com>
2020-01-08 11:34:13 -0600

    update to manifest.json and index.js - SLF

--- server/index.js
+++ server/index.js
@@ -1,27 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
-const PORT = process.env.PORT || 3001;
-
-app.get('/api/greeting', (req, res) => {
-  const name = req.query.name || 'World';
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
-});
+const PORT = process.env.PORT || 3000;
 
 if(process.env.NODE_ENV === 'production'){
     //set static folder
     app.use(express.static('client/build'));
 }
+
 app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile('index.html', { root: path.join(__dirname, '../public') 
+  });
 });
 
 app.listen(PORT, () =>
-  console.log('Express server is running on localhost:3001')
+  console.log('Express server is running on localhost:3000'),
+  console.log(__dirname)
 );
