9c7ad7ebddc00e356a073f27e86997c0bca417db
Stephen Fox <stephen@capablecreative.com>
2020-01-08 16:49:25 -0600

    Test

--- server/index.js
+++ server/index.js
@@ -12,7 +12,8 @@ const PORT = process.env.PORT || 3000;
 
 if(process.env.NODE_ENV === 'production'){
     //set static folder
-    app.use(express.static('index.html', { root: path.join(__dirname, '../build')}))
+    app.use(express.static('../build'));
+}
 
 app.get('*',(req, res) => {
   res.sendFile('index.html', { root: path.join(__dirname, '../build') 
@@ -22,4 +23,4 @@ app.get('*',(req, res) => {
 app.listen(PORT, () =>
   console.log('Express server is running on localhost:3000'),
   console.log(__dirname)
-)};
+);
