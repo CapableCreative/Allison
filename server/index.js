756a0ecfa2365e93d050abbb2171309f5a0a7fd6
Stephen Fox <stephen@capablecreative.com>
2020-01-08 15:28:35 -0600

    Jason Hall Assist - SLF

--- server/index.js
+++ server/index.js
@@ -1,6 +1,23 @@
+
+const path =  require("path");
+const router = require("express").Router();
+const apiRoutes = require("./api");
+
+// API ROUTES
+router.use("/api", apiRoutes);
+
+// If no API routes are hit, send the React app
+router.use((req, res) =>
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+);
+
+module.exports = router;
+
+/*
 const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
+const routes = require('./routes')
 const path = require('path');
 
 const app = express();
@@ -14,6 +31,8 @@ if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
 }
 
+app.use(routes);
+
 app.get('*',(req, res) => {
   res.sendFile('index.html', { root: path.join(__dirname, '../public') 
   });
@@ -23,3 +42,4 @@ app.listen(PORT, () =>
   console.log('Express server is running on localhost:3000'),
   console.log(__dirname)
 );
+*/
